f5cd8952072a9bc2dcb5740afb7b05f46410f217
Eduardo-Malhao <eduardo.malhao@outlook.com>
2024-02-27 19:53:07 -0300

    indexcss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import HeaderProvider from './context/HeaderProvider';
-import './index.css';
+// import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import MealsProvider from './context/MealsProvider';
